4a577cd973244aebc5ba9e6b2750ea8bd2e0c403
jian wei <15667163+wei134103@user.noreply.gitee.com>
2026-06-24 18:20:14 +0800

    build: 升级版本号至1.0.0.7并完善CI流程
    1.  将程序集版本和文件版本从1.0.0.6更新至1.0.0.7
    2.  在GitHub Release CI中添加NuGet包还原步骤
    3.  移除项目中冗余的EntityFramework本地props导入引用

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("1.0.0.6")]
-[assembly: AssemblyFileVersion("1.0.0.6")]
+[assembly: AssemblyVersion("1.0.0.7")]
+[assembly: AssemblyFileVersion("1.0.0.7")]
